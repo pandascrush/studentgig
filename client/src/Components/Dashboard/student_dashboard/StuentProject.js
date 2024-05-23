771b030d29a165f34654f312d95e64938f5e66a0
pandascrush <velayuthamsiva55@gmail.com>
2024-05-23 09:32:46 +0530

    Stduent Projects fixed

--- client/src/Components/Dashboard/student_dashboard/StuentProject.js
+++ client/src/Components/Dashboard/student_dashboard/StuentProject.js
@@ -3,19 +3,17 @@ import React, { useEffect, useState } from "react";
 import { Link, useParams } from "react-router-dom";
 
 function StudentProject() {
-<<<<<<< HEAD
-  const { id } = useParams();  
-=======
   const { id } = useParams();
->>>>>>> 573160953d754583050b6b27aff0273e3a0d8e49
   const [projects, setProjects] = useState([]);
 
   useEffect(() => {
     axios.get(`http://localhost:5000/admin/basproject/${id}`).then((res) => {
-      setProjects(res.data.result.map(project => ({
-        ...project,
-        formatted_expiry_date: formatExpiryDate(project.expiry_date)
-      })));
+      setProjects(
+        res.data.result.map((project) => ({
+          ...project,
+          formatted_expiry_date: formatExpiryDate(project.expiry_date),
+        }))
+      );
     });
   }, [id]);
 
@@ -24,62 +22,55 @@ function StudentProject() {
     let date = new Date(expiryDate);
     let hours = date.getHours();
     let minutes = date.getMinutes();
-    let ampm = hours >= 12 ? 'PM' : 'AM';
+    let ampm = hours >= 12 ? "PM" : "AM";
     hours = hours % 12;
     hours = hours ? hours : 12;
-    minutes = minutes < 10 ? '0' + minutes : minutes;
-    let formattedTime = hours + ':' + minutes + ' ' + ampm;
-    let options = { year: 'numeric', month: 'long', day: 'numeric' };
-    let formattedDate = date.toLocaleDateString('en-US', options);
-    return formattedDate + ' ' + formattedTime;
+    minutes = minutes < 10 ? "0" + minutes : minutes;
+    let formattedTime = hours + ":" + minutes + " " + ampm;
+    let options = { year: "numeric", month: "long", day: "numeric" };
+    let formattedDate = date.toLocaleDateString("en-US", options);
+    return formattedDate + " " + formattedTime;
   };
 
   return (
     <div>
-<<<<<<< HEAD
-     {
-      projects.length === 0 ?(
+      {projects.length === 0 ? (
         <h2>Please Update Your Profile</h2>
-      ):(
+      ) : (
         <table className="table table-hover table-nowrap">
-=======
-      <table className="table table-hover table-nowrap">
->>>>>>> 573160953d754583050b6b27aff0273e3a0d8e49
-        <thead className="thead-light">
-          <tr>
-            <th scope="col">Serial No</th>
-            <th scope="col">Project Name</th>
-            <th scope="col">Project Details</th>
-            <th>Expires In</th>
-          </tr>
-        </thead>
-        <tbody>
-<<<<<<< HEAD
-          {  projects.map((project, index) => (
-=======
-          {projects.map((project, index) => (
->>>>>>> 573160953d754583050b6b27aff0273e3a0d8e49
-            <tr key={index}>
-              <td>{index + 1}</td>
-              <td>{project.project_name}</td>
-              <td>
-<<<<<<< HEAD
-                <Link to={`/detail/${id}/${project.project_id}`} className="btn btn-success">View</Link>
-=======
-                <Link to={`/detail/${project.project_id}`} className="btn btn-success">View</Link>
->>>>>>> 573160953d754583050b6b27aff0273e3a0d8e49
-              </td>
-              <td>{project.formatted_expiry_date}</td>
+          <thead className="thead-light">
+            <tr>
+              <th scope="col">Serial No</th>
+              <th scope="col">Project Name</th>
+              <th scope="col">Project Details</th>
+              <th>Expires In</th>
             </tr>
-          ))}
-        </tbody>
-      </table>
-<<<<<<< HEAD
-      )
-     }
-      
-=======
->>>>>>> 573160953d754583050b6b27aff0273e3a0d8e49
+          </thead>
+          <tbody>
+            {projects.map((project, index) => (
+              <tr key={index}>
+                <td>{index + 1}</td>
+                <td>{project.project_name}</td>
+                <td>
+                  <Link
+                    to={`/detail/${id}/${project.project_id}`}
+                    className="btn btn-success"
+                  >
+                    View
+                  </Link>
+                  <Link
+                    to={`/detail/${project.project_id}`}
+                    className="btn btn-success"
+                  >
+                    View
+                  </Link>
+                </td>
+                <td>{project.formatted_expiry_date}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      )}
     </div>
   );
 }
